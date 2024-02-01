0be5306dd62108f0e779f70f76ac8ced6cd83617
Bikram Das <bikramdas736136@gmail.com>
2024-02-01 14:57:50 +0530

    first-commit

---
+++ logo.js
@@ -0,0 +1,4 @@
+document.getElementById("myButton").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent default form submission
+    window.location.href = "../Landing_Page/index.html";
+});
