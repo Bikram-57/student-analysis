91aff0e85e7c37ff66064d66a969d16d7fea0c4c
Bikram Das <bikramdas736136@gmail.com>
2024-02-01 15:02:03 +0530

    filename updates

--- logo.js
+++ logo.js
@@ -1,4 +1,4 @@
 document.getElementById("myButton").addEventListener("click", function(event) {
     event.preventDefault(); // Prevent default form submission
-    window.location.href = "../Landing_Page/index.html";
+    window.location.href = "../Landing_Page/dashboard.html";
 });
